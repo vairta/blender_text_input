6517995579e9e6d5186b4452018815114b2c6c34
vain <88568787+vairta@users.noreply.github.com>
2023-06-09 16:21:20 +0800

    Create io.py

---
+++ images/io.py
@@ -0,0 +1,8 @@
+# -*- coding: UTF-8 -*-
+
+# 引入所需库
+import bpy
+from bpy.types import Operator, PropertyGroup, UIList, Panel
+from bpy.props import IntProperty, FloatProperty, StringProperty, EnumProperty
+from typing import Collection
+import os, platform
